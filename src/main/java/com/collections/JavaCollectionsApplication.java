388843bb50c2d970cf4dbe11bc7ae741e8e45ab8
sagardhamal5498 <sagardhamal9999@gmail.com>
2024-07-23 10:17:30 +0530

    main project

---
+++ src/main/java/com/collections/JavaCollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCollectionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collections/JavaCollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
