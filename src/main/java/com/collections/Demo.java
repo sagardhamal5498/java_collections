f2676fe193b7d71f9cdd605c25960d0154bd5526
Prajwal <prajwalvkande2001@gmail.com>
2024-07-23 10:25:47 +0530

    new class demo created

---
+++ src/main/java/com/collections/Demo.java
@@ -0,0 +1,8 @@
+package com.collections;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("sugar suggg");
+    }
+}
--- src/main/java/com/collections/JavaCollectionsApplication.java
+++ src/main/java/com/collections/JavaCollectionsApplication.java
@@ -1,13 +1,11 @@
 package com.collections;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class JavaCollectionsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaCollectionsApplication.class, args);
+
+
 	}
 
 }
