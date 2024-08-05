c9e71a2181298361e375a8a16f1b76a24795fc7c
sagardhamal5498 <sagardhamal9999@gmail.com>
2024-08-05 14:09:49 +0530

    Core Java Notes Added

---
+++ src/main/java/com/collections/trywithresource/A.java
@@ -0,0 +1,9 @@
+package com.collections.trywithresource;
+
+public class A {
+
+    A(){
+        System.out.println("Sagar from A");
+    }
+
+}
---
+++ src/main/java/com/collections/trywithresource/B.java
@@ -0,0 +1,8 @@
+package com.collections.trywithresource;
+
+public class B extends A{
+
+    public static void main(String[] args) {
+        B b = new B();
+    }
+}
